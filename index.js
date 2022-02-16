6f6ab2fba47fba8c693c893bcc3b77be8e450c49
Ava <avamsteimle@gmail.com>
2022-02-15 21:31:31 -0800

    mail to added index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var gitHubBtn = $(".gitHub");
 var linkedInBtn = $(".linkedIn");
 var backHomeBtn = $(".backHome");
 
+gmailBtn.on("click", function () {
+    window.location.href = "mailto:avamsteimle@gmail.com?subject=Subject&body=message%20goes%20here";
+});
+
 gitHubBtn.on("click", function () {
     window.location.href = "http://github.com/kreativekntrl";
 });
@@ -10,6 +14,7 @@ gitHubBtn.on("click", function () {
 linkedInBtn.on("click", function () {
     window.location.href = "http://www.linkedin.com/in/avasteimle/";
 });
+
 
 // Scrolls to top of page 
 backHomeBtn.on("click", function() {
